9c2923e77b0147965cb500301669536ad6d551fd
Dylan Davis <djdavis1420@gmail.com>
2019-04-03 19:35:57 -0500

    Added Product Cost Calculation

--- src/models/product.py
+++ src/models/product.py
@@ -3,7 +3,12 @@ class Product:
         self.name = name
         self.standard_price = standard_price
         self.discount_price = discount_price
+        self.sale_price = 0
 
     def set_discount(self, discount_amount):
         self.discount_price = self.standard_price - discount_amount
 
+    def calculate_cost(self, weight):
+        use_price = self.discount_price if self.discount_price is not None else self.standard_price
+        self.sale_price = round((use_price * weight),  2)
+        return self.sale_price
--- test/models/test_product.py
+++ test/models/test_product.py
@@ -8,3 +8,22 @@ def test_set_discount__should_reduce_soup_price_by_twenty_cents():
     actual = soup.discount_price
 
     assert actual == 1.69
+
+
+def test_calculate_cost__should_return_cost_based_on_standard_price_and_weight():
+    beef = Product('beef', 5.99)
+    weight = .75
+
+    actual = beef.calculate_cost(weight)
+
+    assert actual == 4.49
+
+
+def test_calculate_cost__should_return_cost_based_on_discount_price_and_weight():
+    beef = Product('beef', 5.99)
+    beef.set_discount(.50)
+    weight = .75
+
+    actual = beef.calculate_cost(weight)
+
+    assert actual == 4.12
